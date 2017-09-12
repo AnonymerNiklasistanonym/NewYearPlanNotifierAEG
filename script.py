41430ae6429979f16c4214ddc3c8ff83b2478b45
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-09-13 00:23:59 +0200

    Sorted imports and added script descripton

--- script.py
+++ script.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
+"""
+Script that scrapes the website with the year plans of the AlbertEeinsteinGymnasium.
+If desired it sends emails to entered recipients (line 39+) over the SimplifiedGmailApi.
+"""
+
 # My imports (the crawler)
 import json
 import logging
 import os
 import re
+
 import httplib2
 import requests
 from bs4 import BeautifulSoup
+
 
 # Gmail Imports (not important for the actual crawler)
 from SendGmailSimplified.SendGmailSimplified import SimplifiedGmailApi
@@ -17,10 +24,8 @@ HOME_DIR = os.path.expanduser('~')
 
 # change this to the directory your script is: !!!!!!!!!!!!!!!!!
 DIR_OF_SCRIPT = os.path.join(HOME_DIR,
-                             os.path.join("Documents/GitHubBeta", "NewYearPlanNotifierAEG"))
-# Documents
+                             os.path.join("Documents", "NewYearPlanNotifierAEG"))
 # Documents/GitHubBeta
-# PycharmProjects
 
 PATH_FOR_LOG = os.path.join(DIR_OF_SCRIPT, "script.log")
 PATH_OF_DATA_FILE = os.path.join(DIR_OF_SCRIPT, 'data.json')
@@ -40,7 +45,8 @@ URL_OF_WEBSITE = "http://www.aeg-boeblingen.de/index.php/aktuelles/stundenplan"
 try:
     logging.info("Try getting the website " + URL_OF_WEBSITE + ":")
     HTTP = httplib2.Http()
-    HEADERS = {'Cookie': "6024a7383aa153ec184c3ea3382596a7=fli24m6oth1a2p43d8a7d64vs7"}
+    HEADERS = {
+        'Cookie': "6024a7383aa153ec184c3ea3382596a7=fli24m6oth1a2p43d8a7d64vs7"}
     RESPONSE, CONTENT = HTTP.request(URL_OF_WEBSITE, 'GET', headers=HEADERS)
 except requests.HTTPError as err:
     logging.warning(err)
@@ -58,7 +64,8 @@ else:
     # replace all extra lines (\n) with nothing
     SOUP_HTML_DATA = SOUP_HTML_DATA.replace("\n", "")
     # repair all the links so that they are usable
-    SOUP_HTML_DATA = SOUP_HTML_DATA.replace("href=\"/", "href=\"http://www.aeg-boeblingen.de/")
+    SOUP_HTML_DATA = SOUP_HTML_DATA.replace(
+        "href=\"/", "href=\"http://www.aeg-boeblingen.de/")
 
     # Load the old file information if it exists
     if os.path.isfile(PATH_OF_DATA_FILE):
@@ -78,15 +85,16 @@ else:
 
             # algorithm encoding source:
             # http://code.activestate.com/recipes/251871-latin1-to-ascii-the-unicode-hammer/
-            XLATE_TABLE = {0xdf: '&#223;', # ß
-                           0xc4: '&#196;', # Ä
-                           0xd6: '&#214;', # Ö
-                           0xdc: '&#220;', # Ü
-                           0xe4: '&#228;', # ä
-                           0xf6: '&#246;', # ö
-                           0xfc: '&#252;'} # ü
+            XLATE_TABLE = {0xdf: '&#223;',  # ß
+                           0xc4: '&#196;',  # Ä
+                           0xd6: '&#214;',  # Ö
+                           0xdc: '&#220;',  # Ü
+                           0xe4: '&#228;',  # ä
+                           0xf6: '&#246;',  # ö
+                           0xfc: '&#252;'}  # ü
             # source: http://www.idautomation.com/product-support/ascii-chart-char-set.html
-            NON_ASCII_RE = re.compile(u'([\x00-\x7f]+)|([^\x00-\x7f])', re.UNICODE).sub
+            NON_ASCII_RE = re.compile(
+                u'([\x00-\x7f]+)|([^\x00-\x7f])', re.UNICODE).sub
             EMAIL_NEW_DATA = str(NON_ASCII_RE(lambda x: x.group(1) or
                                               XLATE_TABLE.setdefault(ord(x.group(2)),
                                                                      '&#9647;'), SOUP_HTML_DATA))
@@ -99,14 +107,17 @@ else:
             DIR_OF_GMAIL_API_FILES = os.path.join(DIR_OF_SCRIPT,
                                                   os.path.join("SendGmailSimplified",
                                                                "gmail_api_files"))
-            PATH_OF_CLIENT_DATA = os.path.join(DIR_OF_GMAIL_API_FILES, "client_data.json")
-            PATH_OF_CLIENT_SECRET = os.path.join(DIR_OF_GMAIL_API_FILES, "client_secret.json")
+            PATH_OF_CLIENT_DATA = os.path.join(
+                DIR_OF_GMAIL_API_FILES, "client_data.json")
+            PATH_OF_CLIENT_SECRET = os.path.join(
+                DIR_OF_GMAIL_API_FILES, "client_secret.json")
             GMAIL_SERVER = SimplifiedGmailApi(PATH_OF_CLIENT_DATA,
                                               PATH_OF_CLIENT_SECRET,
                                               DIR_OF_GMAIL_API_FILES)
 
             for recipient in RECIPIENTS:
-                GMAIL_SERVER.send_html(recipient, "Neuer Stundenplan?", EMAIL_TEXT)
+                GMAIL_SERVER.send_html(
+                    recipient, "Neuer Stundenplan?", EMAIL_TEXT)
 
         # And save the new information
         with open(PATH_OF_DATA_FILE, 'w') as outfile:
